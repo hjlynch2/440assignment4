5f73b67655958690f12712cdbbd5e0d099638e88
tommypacker <tommypacker@gmail.com>
2016-11-23 16:41:15 -0600

    Added exploration function and set up state-action space

--- part1/pong.py
+++ part1/pong.py
@@ -8,11 +8,18 @@
 
 import pygame, sys, random
 import math
+from random import randint
 from pygame.locals import *
 
 # Number of frames per second
 # Change this value to speed up or slow down your game
 FPS = 20
+
+#Action Constants
+GO_NOWHERE = 0
+GO_UP = 1
+GO_DOWN = 2
+ACTIONS = (GO_NOWHERE, GO_UP, GO_DOWN)
 
 #Global Variables to be used through our program
 
@@ -22,10 +29,14 @@ LINETHICKNESS = 10
 PADDLESIZE = WINDOWHEIGHT * .2
 PADDLEOFFSET = 0
 
+Q_dict = dict() #stores the Q values
+N_dict = dict() #stores the number of times visting state-action
+
 # Set up the colours
 BLACK     = (0  ,0  ,0  )
 RED       = (255,0  ,0  )
 WHITE     = (255,255,255)
+
 
 #Draws the arena the game will be played in. 
 def drawArena():
@@ -87,7 +98,7 @@ def movePaddle(paddle, paddleMovement):
             paddle.y -= 0.04 * WINDOWHEIGHT * (2.0/FPS)
 
 
-#Helper functions for state detection
+##### Helper functions for state detection #####
 def getBallXState(ballDirX):
     if ballDirX < 0:
         return -1
@@ -102,14 +113,98 @@ def getBallYState(ballDirY):
     else:
         return 1
 
-def getPaddleLocation(paddle):
-    paddle_y = float(paddle.y)/400
-    return math.floor((paddle_y * 12)/(0.8))
+def getPaddleLocation(paddle_y_coord):
+    paddle_y = float(paddle_y_coord)/400
+    return int(math.floor((paddle_y * 12)/(0.8)))
+
+def getXCoord(ball):
+    return int(math.floor(ball.x/33.333333))
+
+def getYCoord(ball):
+    return int(math.floor(ball.y/33.333333))
 
 def getStateCoords(ball):
     gridX = math.floor(ball.x/33.333333)
     gridY = math.floor(ball.y/33.333333)
     return gridX + 12 * gridY
+
+def getState(ball, ballDirX, ballDirY, paddle):
+    ball_x = getXCoord(ball)
+    ball_y = getYCoord(ball)
+    velocity_x = getBallXState(ballDirX)
+    velocity_y = getBallYState(ballDirY)
+    paddle_y = getPaddleLocation(paddle.y)
+    return (ball_x, ball_y, velocity_x, velocity_y, paddle_y)
+
+
+##### Exploration Function #####
+NUMBER_EXPLORED_THRESHOLD = 5  #Ne in the lectures
+R_PLUS = 10 #R+ -> arbitrary value (modified utility)
+
+"""
+Return what action to take next
+Arguments: cur_state - tuple representing the current state
+"""
+def exploration_function(cur_state):
+    #Go up
+    go_up_state = cur_state + (GO_UP,) #Equivalent to (s, a')
+    num_visited_up = 0
+    up_utility = R_PLUS
+
+    try:
+        num_visited_up = N_dict[go_up_state]
+    except Exception as e:
+        num_visited_up = 0
+
+    if num_visited_up >= NUMBER_EXPLORED_THRESHOLD:
+        try:
+            up_utility = Q_dict[go_up_state] 
+        except Exception as e:
+            up_utility = R_PLUS
+
+    #Go down
+    go_down_state = cur_state + (GO_DOWN,)
+    num_visited_down = 0
+    down_utility = R_PLUS
+
+    try:
+        num_visited_down = N_dict[go_down_state]
+    except Exception as e:
+        num_visited_down = 0
+
+    if num_visited_down >= NUMBER_EXPLORED_THRESHOLD:
+        try:
+            down_utility = Q_dict[go_down_state] 
+        except Exception as e:
+            down_utility = R_PLUS
+
+    #Do nothing
+    go_nowhere_state = cur_state + (GO_NOWHERE,)
+    num_visited_nowhere = 0
+    nowhere_utility = R_PLUS
+
+    try:
+        num_visited_nowhere = N_dict[go_nowhere_state]
+    except Exception as e:
+        num_visited_nowhere = 0
+
+    if num_visited_nowhere >= NUMBER_EXPLORED_THRESHOLD:
+        try:
+            nowhere_utility = Q_dict[go_nowhere_state] 
+        except Exception as e:
+            nowhere_utility = R_PLUS
+
+    # Choose random direction when all the utilities are the same
+    if up_utility == down_utility and down_utility == nowhere_utility:
+        rand_index = randint(0,2)
+        return ACTIONS[rand_index]
+    best_utility = max(up_utility, down_utility, nowhere_utility)
+    if best_utility == up_utility:
+        return GO_UP
+    elif best_utility == down_utility:
+        return GO_DOWN
+    elif best_utility == nowhere_utility:
+        return GO_NOWHERE
 
 
 #Main function
@@ -169,9 +264,19 @@ def main():
             ballDirX, ballDirY = checkEdgeCollision(ball, ballDirX, ballDirY)
         ball = moveBall(ball, ballDirX, ballDirY)
 
+
+
+        state = getState(ball, ballDirX, ballDirY, paddle)
+        next_action = exploration_function(state)
+        
+
         # randomly move the paddle for now
         paddleMovement = random.random()
         movePaddle(paddle, paddleMovement)
+
+
+
+
 
         pygame.display.update()
         FPSCLOCK.tick(FPS)
